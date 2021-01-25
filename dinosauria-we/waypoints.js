c04195869f1ad42f5e1582c6ff7cdc63f060bce8
jordan paplow <jordanpaplow@gmail.com>
2021-01-25 15:19:56 -0800

    progress

--- dinosauria-we/waypoints.js
+++ dinosauria-we/waypoints.js
@@ -18,8 +18,227 @@ $textfade2.waypoint(function(direction) {
     }
 }, { offset:'70%'});
 
+var $textfade3 = $('.textfade3');
+
+$textfade3.waypoint(function(direction) {
+    if (direction == 'down') {
+        $textfade3.addClass('js-textfade-animate');
+    } else {
+    $textfade3.removeClass('js-textfade-animate');
+    }
+}, { offset:'70%'});
+
+var $textfade4 = $('.textfade4');
+
+$textfade4.waypoint(function(direction) {
+    if (direction == 'down') {
+        $textfade4.addClass('js-textfade-animate');
+    } else {
+    $textfade4.removeClass('js-textfade-animate');
+    }
+}, { offset:'70%'});
+
+var $textfade5 = $('.textfade5');
+
+$textfade5.waypoint(function(direction) {
+    if (direction == 'down') {
+        $textfade5.addClass('js-textfade-animate');
+    } else {
+    $textfade5.removeClass('js-textfade-animate');
+    }
+}, { offset:'70%'});
+
+var $textfade6 = $('.textfade6');
+
+$textfade6.waypoint(function(direction) {
+    if (direction == 'down') {
+        $textfade6.addClass('js-textfade-animate');
+    } else {
+    $textfade6.removeClass('js-textfade-animate');
+    }
+}, { offset:'70%'});
+
+var $textfade7 = $('.textfade7');
+
+$textfade7.waypoint(function(direction) {
+    if (direction == 'down') {
+        $textfade7.addClass('js-textfade-animate');
+    } else {
+    $textfade7.removeClass('js-textfade-animate');
+    }
+}, { offset:'70%'});
+
+var $textfade8 = $('.textfade8');
+
+$textfade8.waypoint(function(direction) {
+    if (direction == 'down') {
+        $textfade2.addClass('js-textfade-animate');
+    } else {
+    $textfade8.removeClass('js-textfade-animate');
+    }
+}, { offset:'70%'});
+
+var $textfade9 = $('.textfade9');
+
+$textfade9.waypoint(function(direction) {
+    if (direction == 'down') {
+        $textfade9.addClass('js-textfade-animate');
+    } else {
+    $textfade9.removeClass('js-textfade-animate');
+    }
+}, { offset:'70%'});
+
+var $textfade10 = $('.textfade10');
+
+$textfade10.waypoint(function(direction) {
+    if (direction == 'down') {
+        $textfade10.addClass('js-textfade-animate');
+    } else {
+    $textfade10.removeClass('js-textfade-animate');
+    }
+}, { offset:'70%'});
+
+var $textfade11 = $('.textfade11');
+
+$textfade11.waypoint(function(direction) {
+    if (direction == 'down') {
+        $textfade11.addClass('js-textfade-animate');
+    } else {
+    $textfade11.removeClass('js-textfade-animate');
+    }
+}, { offset:'70%'});
+
+var $textfade12 = $('.textfade12');
+
+$textfade12.waypoint(function(direction) {
+    if (direction == 'down') {
+        $textfade12.addClass('js-textfade-animate');
+    } else {
+    $textfade12.removeClass('js-textfade-animate');
+    }
+}, { offset:'70%'});
+
+/*Image fading */
 
 
+var $imgfade = $('.imgfade');
+
+$imgfade.waypoint(function(direction) {
+    if (direction == 'down') {
+        $imgfade.addClass('js-imgfade-animate');
+    } else {
+    $imgfade.removeClass('js-imgfade-animate');
+    }
+}, { offset:'50%'});
+
+var $imgfade2 = $('.imgfade2');
+
+$imgfade2.waypoint(function(direction) {
+    if (direction == 'down') {
+        $imgfade2.addClass('js-imgfade-animate');
+    } else {
+    $imgfade2.removeClass('js-imgfade-animate');
+    }
+}, { offset:'50%'});
+
+var $imgfade3 = $('.imgfade3');
+
+$imgfade3.waypoint(function(direction) {
+    if (direction == 'down') {
+        $imgfade3.addClass('js-imgfade-animate');
+    } else {
+    $imgfade3.removeClass('js-imgfade-animate');
+    }
+}, { offset:'50%'});
+
+var $imgfade4 = $('.imgfade4');
+
+$imgfade4.waypoint(function(direction) {
+    if (direction == 'down') {
+        $imgfade4.addClass('js-imgfade-animate');
+    } else {
+    $imgfade4.removeClass('js-imgfade-animate');
+    }
+}, { offset:'50%'});
+
+var $imgfade5 = $('.imgfade5');
+
+$imgfade5.waypoint(function(direction) {
+    if (direction == 'down') {
+        $imgfade5.addClass('js-imgfade-animate');
+    } else {
+    $imgfade.removeClass('js-imgfade-animate');
+    }
+}, { offset:'50%'});
+
+var $imgfade6 = $('.imgfade6');
+
+$imgfade6.waypoint(function(direction) {
+    if (direction == 'down') {
+        $imgfade6.addClass('js-imgfade-animate');
+    } else {
+    $imgfade6.removeClass('js-imgfade-animate');
+    }
+}, { offset:'50%'});
+
+var $imgfade7 = $('.imgfade7');
+
+$imgfade7.waypoint(function(direction) {
+    if (direction == 'down') {
+        $imgfade7.addClass('js-imgfade-animate');
+    } else {
+    $imgfade7.removeClass('js-imgfade-animate');
+    }
+}, { offset:'50%'});
 
 
+var $imgfade8 = $('.imgfade88');
 
+$imgfade8.waypoint(function(direction) {
+    if (direction == 'down') {
+        $imgfade8.addClass('js-imgfade-animate');
+    } else {
+    $imgfade8.removeClass('js-imgfade-animate');
+    }
+}, { offset:'50%'});
+
+var $imgfade9 = $('.imgfade9');
+
+$imgfade9.waypoint(function(direction) {
+    if (direction == 'down') {
+        $imgfade9.addClass('js-imgfade-animate');
+    } else {
+    $imgfade9.removeClass('js-imgfade-animate');
+    }
+}, { offset:'50%'});
+
+var $imgfade10 = $('.imgfade10');
+
+$imgfade10.waypoint(function(direction) {
+    if (direction == 'down') {
+        $imgfade10.addClass('js-imgfade-animate');
+    } else {
+    $imgfade10.removeClass('js-imgfade-animate');
+    }
+}, { offset:'50%'});
+
+var $imgfade11 = $('.imgfade11');
+
+$imgfade11.waypoint(function(direction) {
+    if (direction == 'down') {
+        $imgfade11.addClass('js-imgfade-animate');
+    } else {
+    $imgfade11.removeClass('js-imgfade-animate');
+    }
+}, { offset:'50%'});
+
+
+var $imgfade12 = $('.imgfade12');
+
+$imgfade12.waypoint(function(direction) {
+    if (direction == 'down') {
+        $imgfade12.addClass('js-imgfade-animate');
+    } else {
+    $imgfade12.removeClass('js-imgfade-animate');
+    }
+}, { offset:'50%'});
